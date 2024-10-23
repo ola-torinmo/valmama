78b1b185eb352bf49107578d4e5204bb82248494
torinmo <torinmo.work@gmailcom>
2024-10-23 13:55:29 +0100

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function App() {
           <img 
             src={celebrant}
             alt="celebrant"
-            className="rounded-lg shadow-xl bg-auto bg-no-repeat bg-center w-[280px] mt-[70px]"
+            className="rounded-lg shadow-xl bg-auto bg-no-repeat bg-center md:w-[280px] w-[250px] md:mt-[70px]"
           />
           
         </div>
